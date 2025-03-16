cf5a36ecfb50b96ca5ae3a659ff24021cc138f83
Desmond Mack <desi.p.mack@gmail.com>
2025-03-15 18:07:03 -0700

    uopdate sender

--- sender2.py
+++ sender2.py
@@ -17,44 +17,43 @@ MORSE_CODE_DICT = {
 }
 
 # Settings
-frequency = 600        # Hz
-dot_duration = 0.1     # Duration of a dot in seconds
-samplerate = 44100     # Audio sample rate
+frequency = 600  # Hz tone frequency (typical Morse frequency)
+dot_duration = 0.1  # Seconds per dot
+
 
 def text_to_morse(text):
     return ' '.join(MORSE_CODE_DICT.get(i.upper(), '') for i in text)
 
+
 def generate_tone(duration):
+    samplerate = 44100
     t = np.linspace(0, duration, int(samplerate * duration), False)
     tone = np.sin(frequency * t * 2 * np.pi)
     return tone
 
-def generate_silence(duration):
-    return np.zeros(int(samplerate * duration))
-
-def play_audio(audio):
-    sd.play(audio, samplerate=samplerate)
-    sd.wait()
 
 def play_morse(morse_code):
-    # Initial silence (critical fix)
-    play_audio(generate_silence(0.5))
-    time.sleep(0.1)  # Slight pause after silence
+    # Send three dots first to prime audio hardware
+    initial_signal = '... / '
+    morse_code = initial_signal + morse_code
 
     for symbol in morse_code:
         if symbol == '.':
             tone = generate_tone(dot_duration)
-            play_audio(tone)
+            sd.play(tone, samplerate=44100)
+            sd.wait()
             time.sleep(dot_duration)
         elif symbol == '-':
             tone = generate_tone(dot_duration * 3)
-            play_audio(tone)
+            sd.play(tone, samplerate=44100)
+            sd.wait()
             time.sleep(dot_duration)
         elif symbol == ' ':
             time.sleep(dot_duration * 3)
         elif symbol == '/':
             time.sleep(dot_duration * 7)
         time.sleep(dot_duration)
+
 
 if __name__ == "__main__":
     message = input("Enter the message to send in Morse: ")
